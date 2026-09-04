4d85af7302f7555a68a92f6c180297038ecfafbe
ardhadyota <ardha.dyota@argapura.com>
2026-09-04 09:59:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -659,7 +659,7 @@ if uploaded_file is not None:
             "Unplanned Downtime",
             "Setup & Adjustment",
             "Idling & Minor Stops",
-            "Slow Cycles",
+            "Slow Cycles", # Diubah dari "Reduced Speed"
             "Process Defects",
             "Startup Losses",
             "Planned Shutdown (Non-OEE)",

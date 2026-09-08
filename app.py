9a31add4224419f629324acde4540a0ac7b0bd9f
ardhadyota <ardha.dyota@argapura.com>
2026-09-08 11:33:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,6 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
-import streamlit as st
-import pandas as pd
-from supabase import create_client, Client
-
-# Koneksi ke Supabase via Streamlit Secrets
-SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # 1. KONFIGURASI HALAMAN DAN STYLES
 st.set_page_config(
@@ -431,11 +423,11 @@ if uploaded_file is not None:
             unsafe_allow_html=True,
         )
         m2.markdown(
-            f'<div class="status-card-warning"><h4 style="color:#F59E0B;margin:0;">🟡 Warning Zone</h4><h2 style="color:#FFF;margin:5px 0;">{count_warning} Line</h2><p style="font-size:0.8rem;color:#9CA3AF;margin:0;">OEE &lt; Target (Perhatian Spv & FM)</p></div>',
+            f'<div class="status-card-warning"><h4 style="color:#F59E0B;margin:0;">🟡 Warning Zone</h4><h2 style="color:#FFF;margin:5px 0;">{count_warning} Line</h2><p style="font-size:0.8rem;color:#9CA3AF;margin:0;">OEE &lt; Target (Perhatian Supervisor)</p></div>',
             unsafe_allow_html=True,
         )
         m3.markdown(
-            f'<div class="status-card-ontrack"><h4 style="color:#10B981;margin:0;">🟢 On Track</h4><h2 style="color:#FFF;margin:5px 0;">{count_ontrack} Line</h2><p style="font-size:0.8rem;color:#9CA3AF;margin:0;">OEE &ge; Target (Sesuai Standard Operasional)</p></div>',
+            f'<div class="status-card-ontrack"><h4 style="color:#10B981;margin:0;">🟢 On Track</h4><h2 style="color:#FFF;margin:5px 0;">{count_ontrack} Line</h2><p style="font-size:0.8rem;color:#9CA3AF;margin:0;">OEE &ge; Target (Sesuai Standar Operasional)</p></div>',
             unsafe_allow_html=True,
         )
 
@@ -525,7 +517,7 @@ if uploaded_file is not None:
         else:
             st.success(
                 f"**STATUS: {status_text} — Line: {selected_line}**\n\n"
-                f"**Performa Operasional Melebihi Target** (Aktual: {avg_oee:.2f}% vs Target: {active_std['oee']:.2f}%)\n\n"
+                f"**Performa Operasional Memenuhi / Melebihi Target** (Aktual: {avg_oee:.2f}% vs Target: {active_std['oee']:.2f}%)\n\n"
                 f"Catatan Operasional: Pertahankan performa operasional & kepatuhan Preventive Maintenance."
             )
 

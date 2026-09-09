7ca1d652c022af563879f718d32c58fd2a78bfb1
ardhadyota <ardha.dyota@argapura.com>
2026-09-09 13:45:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1126,7 +1126,7 @@ if uploaded_file is not None:
         if not underperforming.empty and not hidden_loss.empty:
             worst_r = underperforming.iloc[0]['LineID']
             worst_l = hidden_loss.iloc[0]['LineID']
-            header_status = f"Peringatan Sistem: Defisit Pencapaian pada {worst_r} & Inefisiensi Terselubung pada {worst_l}"
+            header_status = f"Peringatan Sistem! Defisit Pencapaian pada {worst_r} & Inefisiensi Terselubung pada {worst_l}"
             desc_status = f"Terdapat **{len(underperforming)} lini** yang belum memenuhi sasaran OEE. Selain itu, terdeteksi **{len(hidden_loss)} lini** yang memenuhi sasaran tetapi memiliki waktu terbuang sangat tinggi (indikasi standar sasaran terlalu rendah)."
         elif not underperforming.empty:
             header_status = f"Prioritaskan Perbaikan Lini {underperforming.iloc[0]['LineID']}"

d3dc773ec4b23b4452c65fa5e7835c3ee2a383a2
ardhadyota <ardha.dyota@argapura.com>
2026-09-14 14:39:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1191,7 +1191,7 @@ if uploaded_file is not None:
                     ai_rec = f"Fokus **Setup & Adjustment (Dandori)** terhitung tinggi sebesar **{setup:.1f} Menit**. Terapkan metode SMED untuk percepat pergantian cetakan."
             elif worst_factor == 'Performance':
                 if idling > 0:
-				    ai_rec = f"Fokus **Speed Losses / Minor Stoppages** terbuang **{idling:.1f} Menit**. Cek sensor *feeding*, komponen aus, atau *micro-stops* di area penggerak."
+                    ai_rec = f"Fokus **Speed Losses / Minor Stoppages** terbuang **{idling:.1f} Menit**. Cek sensor *feeding*, komponen aus, atau *micro-stops* di area penggerak."
                 else:
                     ai_rec = f"Fokus **Reject / Quality Loss** terakumulasi **{int(defect):,} pcs**. Lakukan re-kalibrasi suhu/tekanan dan validasi *incoming material*."
                 

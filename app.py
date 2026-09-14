fac79766051e8f41c0eaa9ccf5358d1093a3fe89
ardhadyota <ardha.dyota@argapura.com>
2026-09-14 10:43:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
 	
 # 1. KONFIGURASI HALAMAN DAN STYLES
